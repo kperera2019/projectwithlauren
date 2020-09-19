d42b916303e4d7fbf84ef2cdb52ea76ebce03bcf
lf-arch <laurencarolfrey@gmail.com>
2020-09-19 18:59:18 -0400

    update intro.py with introduction to booleans
    some fun boolean algebra (read DeMorgan's Law)

--- intro.py
+++ intro.py
@@ -1,3 +1,9 @@
 #hello world test program
 
+#methods, and parameters. 
 print("This is the first iteration that we'll develop together.")
+
+#exercise in booleans
+kevinisdev = True
+laurenisdev = True
+print(not(not(laurenisdev)))
